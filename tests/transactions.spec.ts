b0c7a3b16babd3ad963ec1dde90ba95854751956
Janaina C <janainacoelhodr@gmail.com>
2024-02-27 19:20:34 -0300

    test: add more tests to transactions

--- tests/transactions.spec.ts
+++ tests/transactions.spec.ts
@@ -31,5 +31,20 @@ test.describe('Transactions', () => {
     await transactionPage.performTransaction('800002', -2);
     await transactionPage.expectDialogWithMessage("From Account and To Account fields cannot be the same.");
   });
+
+  test('High Value Transaction', async () => {
+    await transactionPage.performTransaction('800003', 9999999);
+    await transactionPage.verifyTransactionSuccess(9999999, '800003', '800002');
+  });
+
+  test('Non-Numeric Transaction Value', async () => {
+    await transactionPage.performTransaction('800003', 'abc');
+    await transactionPage.expectDialogWithMessage("Transfer Amount must be a number greater than 0.");
+  });
+
+  test('Empty Value Transaction', async () => {
+    await transactionPage.performTransaction('800003', '');
+    await transactionPage.expectDialogWithMessage("Transfer Amount must be a number greater than 0.");
+});
 });
 
